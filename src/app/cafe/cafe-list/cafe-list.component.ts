f2a3fb2ae5bf9350235d42e98c1520f3c9294fa3
dacperezce96 <dc.perezc1@uniandes.edu.co>
2022-04-29 22:32:05 -0500

    contabilizar tipos de cafe

--- src/app/cafe/cafe-list/cafe-list.component.ts
+++ src/app/cafe/cafe-list/cafe-list.component.ts
@@ -10,17 +10,34 @@ import { CafeService } from '../cafe.service';
 export class CafeListComponent implements OnInit {
 
   cafes: Cafe[] = [];
+  nroBlend: number = 0;
+  nroOrigen: number = 0;
 
   constructor(private cafeService: CafeService) { }
 
   getCafes(): void{
     this.cafeService.getCafes().subscribe((cafes)=>{
       this.cafes = cafes;
+      this.getNroTipos();
     });
+  }
+
+  getNroTipos(): void{
+    this.cafes.forEach(c => {
+      if(c.tipo === "Blend"){
+        this.nroBlend+=1;
+      }else if(c.tipo === "Café de Origen"){
+        this.nroOrigen+=1;
+      }
+      //console.log(c.id);
+    });
+    //console.log(`le numero de blend es ${this.nroBlend}`);
+    //console.log(`le numero de origen es ${this.nroOrigen}`);
   }
 
   ngOnInit() {
     this.getCafes();
   }
+
 
 }
