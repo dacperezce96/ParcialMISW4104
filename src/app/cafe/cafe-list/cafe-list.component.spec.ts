0966037e8862e9e10aab4494ff665b04977fb375
dacperezce96 <dc.perezc1@uniandes.edu.co>
2022-04-29 23:57:20 -0500

    prueba de tabla

--- src/app/cafe/cafe-list/cafe-list.component.spec.ts
+++ src/app/cafe/cafe-list/cafe-list.component.spec.ts
@@ -2,18 +2,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 import { CafeListComponent } from './cafe-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CafeService } from '../cafe.service'
+import { Cafe } from '../cafe'
 
 describe('CafeListComponent', () => {
   let component: CafeListComponent;
   let fixture: ComponentFixture<CafeListComponent>;
+  let debug: DebugElement;
+  let table: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
-      declarations: [ CafeListComponent ]
+      declarations: [ CafeListComponent ],
+      providers: [CafeService]
     })
     .compileComponents();
   }));
@@ -21,10 +27,53 @@ describe('CafeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CafeListComponent);
     component = fixture.componentInstance;
+
+    component.cafes = [
+      new Cafe(
+        faker.datatype.number(),
+        faker.commerce.productName(),
+        faker.lorem.words(2),
+        faker.address.city(),
+        faker.commerce.productName(),
+        faker.datatype.number(),
+        faker.image.imageUrl()
+      ),
+      new Cafe(
+        faker.datatype.number(),
+        faker.commerce.productName(),
+        faker.lorem.words(2),
+        faker.address.city(),
+        faker.commerce.productName(),
+        faker.datatype.number(),
+        faker.image.imageUrl()
+      ),
+      new Cafe(
+        faker.datatype.number(),
+        faker.commerce.productName(),
+        faker.lorem.words(2),
+        faker.address.city(),
+        faker.commerce.productName(),
+        faker.datatype.number(),
+        faker.image.imageUrl()
+      )
+    ];
+
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
+
+    table = fixture.nativeElement.querySelector('table');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should table has one rows of head", () => {
+    expect(table.children[0].childElementCount).toEqual(1);
+  });
+
+  it("should table has three rows of coffes", () => {
+    expect(table.children[1].childElementCount).toEqual(3);
   });
 });
