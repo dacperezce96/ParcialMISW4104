d99008f4cff6e549710dc4f7c5b9d8f62f7fb6e9
dacperezce96 <dc.perezc1@uniandes.edu.co>
2022-04-29 22:55:01 -0500

    listo para pruebas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CafeListComponent } from './cafe/cafe-list/cafe-list.component'
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      imports: [RouterTestingModule, HttpClientModule],
+      declarations: [AppComponent, CafeListComponent],
     }).compileComponents();
   });
 
